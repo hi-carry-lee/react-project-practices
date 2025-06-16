d74655339062ab3d829895df886fedce904cf3f2
liren <327381633@qq.com>
2025-06-17 06:24:11 +0800

    project5: refactor code and add some comments

--- projects/5-twitter-clone/frontend/README.md
+++ projects/5-twitter-clone/frontend/README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# record
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+1. add some property in index.css to fix the layout shift issue
+   when notification page doesn't have scroll bar and other page has, then switch between them the whole layout will shift, since the scroll bar occupy a tiny width;
+   by adding some property to force the scroll bar appear in notification page, it fix the problem
--- projects/5-twitter-clone/frontend/src/components/common/RightPanel.jsx
+++ projects/5-twitter-clone/frontend/src/components/common/RightPanel.jsx
@@ -1,82 +1,115 @@
-import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-
-import useFollow from "../../hooks/useFollow";
-
-import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import LoadingSpinner from "./LoadingSpinner";
+import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import useFollow from '../../hooks/useFollow';
+import RightPanelSkeleton from '../skeletons/RightPanelSkeleton';
+import LoadingSpinner from './LoadingSpinner';
 
 const RightPanel = () => {
-	const { data: suggestedUsers, isLoading } = useQuery({
-		queryKey: ["suggestedUsers"],
-		queryFn: async () => {
-			try {
-				const res = await fetch("/api/users/suggested");
-				const data = await res.json();
-				if (!res.ok) {
-					throw new Error(data.error || "Something went wrong!");
-				}
-				return data;
-			} catch (error) {
-				throw new Error(error.message);
-			}
-		},
-	});
+  const { data: suggestedUsers, isLoading } = useQuery({
+    queryKey: ['suggestedUsers'],
+    queryFn: async () => {
+      try {
+        const res = await fetch('/api/users/suggested');
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || 'Something went wrong!');
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+  });
 
-	const { follow, isPending } = useFollow();
+  const { follow, isPending } = useFollow();
 
-	if (suggestedUsers?.length === 0) return <div className='md:w-64 w-0'></div>;
+  if (suggestedUsers?.length === 0) return <div className='md:w-64 w-0'></div>;
 
-	return (
-		<div className='hidden lg:block my-4 mx-2'>
-			<div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
-				<p className='font-bold'>Who to follow</p>
-				<div className='flex flex-col gap-4'>
-					{/* item */}
-					{isLoading && (
-						<>
-							<RightPanelSkeleton />
-							<RightPanelSkeleton />
-							<RightPanelSkeleton />
-							<RightPanelSkeleton />
-						</>
-					)}
-					{!isLoading &&
-						suggestedUsers?.map((user) => (
-							<Link
-								to={`/profile/${user.username}`}
-								className='flex items-center justify-between gap-4'
-								key={user._id}
-							>
-								<div className='flex gap-2 items-center'>
-									<div className='avatar'>
-										<div className='w-8 rounded-full'>
-											<img src={user.profileImg || "/avatar-placeholder.png"} />
-										</div>
-									</div>
-									<div className='flex flex-col'>
-										<span className='font-semibold tracking-tight truncate w-28'>
-											{user.fullName}
-										</span>
-										<span className='text-sm text-slate-500'>@{user.username}</span>
-									</div>
-								</div>
-								<div>
-									<button
-										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => {
-											e.preventDefault();
-											follow(user._id);
-										}}
-									>
-										{isPending ? <LoadingSpinner size='sm' /> : "Follow"}
-									</button>
-								</div>
-							</Link>
-						))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='hidden lg:flex flex-col  my-4 mx-2'>
+      <div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
+        <p className='font-bold text-xl mb-2 text-center'>Who to follow</p>
+        <div className='flex flex-col gap-4'>
+          {/* loading skeleton */}
+          {isLoading && (
+            <>
+              <RightPanelSkeleton />
+              <RightPanelSkeleton />
+              <RightPanelSkeleton />
+              <RightPanelSkeleton />
+            </>
+          )}
+
+          {/* users waiting for follow */}
+          {!isLoading &&
+            suggestedUsers?.map((user) => (
+              <Link
+                to={`/profile/${user.username}`}
+                className='flex items-center justify-between gap-4 hover:bg-[#3d4044] transition-all duration-200 rounded-full p-2 group'
+                key={user._id}
+              >
+                <div className='flex gap-2 items-center'>
+                  <div className='avatar'>
+                    <div className='w-8 rounded-full'>
+                      <img src={user.profileImg || '/avatar-placeholder.png'} />
+                    </div>
+                  </div>
+                  <div className='flex flex-col'>
+                    <span className='font-semibold tracking-tight truncate w-28'>
+                      {user.fullName}
+                    </span>
+                    <span
+                      // ! add a group hover effect
+                      className='text-sm text-slate-500 group-hover:text-slate-300 transition-colors duration-200'
+                    >
+                      @{user.username}
+                    </span>
+                  </div>
+                </div>
+                <button
+                  className='btn bg-slate-300 text-black hover:bg-slate-100 hover:opacity-90 rounded-full btn-sm'
+                  onClick={(e) => {
+                    e.preventDefault();
+                    follow(user._id);
+                  }}
+                >
+                  {isPending ? <LoadingSpinner size='sm' /> : 'Follow'}
+                </button>
+              </Link>
+            ))}
+        </div>
+      </div>
+
+      <div className='mt-auto bg-[#16181C] p-4 rounded-md sticky bottom-32'>
+        <h2 className='font-bold text-xl mb-2 text-center'>Trending News</h2>
+        <ul className='flex flex-col gap-4'>
+          <li className='flex items-center'>
+            <Link
+              to='/news/1'
+              className='hover:bg-[#3d4044] transition-all duration-200 rounded-full px-2 py-1'
+            >
+              Trending news 1
+            </Link>
+          </li>
+          <li className='flex items-center'>
+            <Link
+              to='/news/1'
+              className='hover:bg-[#3d4044] transition-all duration-200 rounded-full px-2 py-1'
+            >
+              Trending news 1
+            </Link>
+          </li>
+          <li className='flex items-center'>
+            <Link
+              to='/news/1'
+              className='hover:bg-[#3d4044] transition-all duration-200 rounded-full px-2 py-1'
+            >
+              Trending news 1
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default RightPanel;
